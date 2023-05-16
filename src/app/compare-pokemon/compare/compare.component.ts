f26d963acba058fd6dd07573e6f99b5ec11651ca
Johan Sebastián Giraldo Rubio <j.sebasgr036@gmail.com>
2023-05-16 17:17:34 -0500

    Add style to select in compare-pokemon

--- src/app/compare-pokemon/compare/compare.component.ts
+++ src/app/compare-pokemon/compare/compare.component.ts
@@ -1,8 +1,8 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
 import { PokemonPaginationItem } from 'src/app/shared/models/pokemon-pagination-item.model';
 import { Pokemon } from 'src/app/shared/models/pokemon.model';
 import { ComparePokemonService } from 'src/app/shared/services/compare-pokemon.service';
+import { capitalizeText } from 'src/app/util';
 
 @Component({
   selector: 'app-compare',
@@ -13,7 +13,7 @@ export class CompareComponent implements OnInit {
   @Input() pokemonId: string
   @Output() onPokemonChanged = new EventEmitter<{ oldId: string, newId: string }>()
   pokemons: PokemonPaginationItem[]
-  selectedPokemon
+  selectedPokemon: PokemonPaginationItem
   pokemon: Pokemon
 
   constructor(private compareService: ComparePokemonService) { }
@@ -28,18 +28,22 @@ export class CompareComponent implements OnInit {
     this.compareService.fetchPokemon(this.pokemonId)
       .subscribe(
         (pokemon: Pokemon) => {
-          this.selectedPokemon = `https://pokeapi.co/api/v2/pokemon/${this.pokemonId}/`
+          this.selectedPokemon = {name: pokemon.name, url: `https://pokeapi.co/api/v2/pokemon/${this.pokemonId}/`}
           this.pokemon = pokemon
         }
       )
   }
 
   onChange(event) {
-    this.onPokemonChanged.emit({ oldId: this.pokemonId, newId: this.getId(event.value) })
+    this.onPokemonChanged.emit({ oldId: this.pokemonId, newId: this.getId(event.value.url) })
   }
 
   getId(value: string) {
     let id = value.split('/')
     return id[id.length - 2]
+  }
+
+  getCapitalizedName(name: string) {
+    return capitalizeText(name)
   }
 }
--- src/app/shared/services/compare-pokemon.service.ts
+++ src/app/shared/services/compare-pokemon.service.ts
@@ -10,38 +10,12 @@ export class ComparePokemonService {
     readonly TYPE_A = 1
     readonly TYPE_B = 2
     private pokemons: Observable<any>
-    private pokemonA = new BehaviorSubject<Pokemon>(null)
-    private pokemonB = new BehaviorSubject<Pokemon>(null)
 
     constructor(private http: HttpClient) { }
-
-    // fetchPokemons() {
-    //     this.http.get<any>(`${this.BASE_URL}/pokemon/?limit=${this.POKEMONS_LIMIT}`)
-    //         .pipe(
-    //             map((pokemons) => pokemons.results.map((pokemon) => {
-    //                 this.fetchPokemon(pokemon.url)
-    //                     .subscribe(
-    //                         (pokemon: Pokemon) => console.log(pokemon)
-    //                     )
-    //             }))
-    //         )
-    //         .subscribe(
-    //             (pokemons: Pokemon[]) => {
-    //                 this.pokemons.next(pokemons)
-    //             }
-    //         )
-    // }
 
     fetchPokemonPagination() {
         this.pokemons = this.http.get<any>(`${this.BASE_URL}/pokemon/?limit=${this.POKEMONS_LIMIT}`)
     }
-
-    // fetchPokemon(url: string): Observable<Pokemon> {
-    //     return this.http.get<Pokemon>(url)
-    //         .pipe(
-    //             map((pokemon: Pokemon) => new Pokemon(pokemon))
-    //         )
-    // }
 
     fetchPokemon(pokemonId: string): Observable<Pokemon> {
         return this.http.get<Pokemon>(`${this.BASE_URL}/pokemon/${pokemonId}`)
@@ -53,30 +27,4 @@ export class ComparePokemonService {
     getPokemons() {
         return this.pokemons
     }
-
-    private getPokemonBehaviourSubject(type: number): BehaviorSubject<Pokemon> {
-        if (type === this.TYPE_A) {
-            return this.pokemonA
-        }
-        else if (type === this.TYPE_B) {
-            return this.pokemonB
-        }
-    }
-
-    // updatePokemon(type: number, url: string) {
-    //     this.fetchPokemon(url)
-    //         .subscribe(
-    //             (pokemon: Pokemon) => {
-    //                 this.getPokemonBehaviourSubject(type).next(pokemon)
-    //             }
-    //         )
-    // }
-
-    getPokemon(type: number): Observable<Pokemon> {
-        return this.getPokemonBehaviourSubject(type).asObservable()
-    }
-
-    // getPokemons(): Observable<Pokemon[]> {
-    //     return this.pokemons.asObservable()
-    // }
 }
