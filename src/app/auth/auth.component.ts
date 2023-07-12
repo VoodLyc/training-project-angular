b8d03bfd6919b9d076f1a62a36232ad1d9de0667
Johan Giraldo Rubio <johan.giraldor@perficient.com>
2023-07-12 09:54:41 -0500

    Add login and signup screen

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
+  isLoginMode: boolean = true
+  isLoginModeSubject: BehaviorSubject<boolean> = new BehaviorSubject(true)
+  authForm: FormGroup
 
+  onSwitchMode() {
+    this.isLoginModeSubject.next(!this.isLoginModeSubject.value)
+  }
+
+  ngOnInit(): void {
+    this.authForm = new FormGroup({
+      'email': new FormControl(null, Validators.required),
+      'password': new FormControl(null, Validators.required)
+    })
+    this.isLoginModeSubject.subscribe(
+      (isLoginMode: boolean) => {
+        this.isLoginMode = isLoginMode
+        this.switchValidators()
+      }
+    )
+  }
+
+  switchValidators() {
+    this.authForm.reset()
+    let emailControl = this.authForm.get('email')
+    let passwordControl = this.authForm.get('password')
+    if (this.isLoginMode) {
+      emailControl.setValidators(Validators.required)
+      passwordControl.setValidators(Validators.required)
+    }
+    else {
+      emailControl.setValidators([Validators.required, Validators.maxLength(50)])
+      passwordControl.setValidators([Validators.required, Validators.minLength(8), Validators.maxLength(50)])
+    }
+  }
 }
